d32d7e797140c135b4c157c2112663672e4cb3d6
Jason Li <jasonli99193@gmail.com>
2023-09-13 21:33:50 -0500

    eventview

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
 import { PostList } from "../components/post/PostList.js"
+import { EventList } from "../components/event/EventList.js"
 
 
 export const ApplicationViews = () => {
@@ -11,6 +12,7 @@ export const ApplicationViews = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/posts" element={<PostList/>} />
+            <Route path="/events" element={<EventList/>} />
             <Route element={<Authorized />}>
                 {/* Add Routes here */}
             </Route>
